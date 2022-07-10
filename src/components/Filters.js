110d577d3d214f1e4ec26f465ad616c823cabc91
Veronica isla <veroislagleaniz@gmail.com>
2022-07-10 14:24:19 +0200

    added favicon

--- src/components/Filters.js
+++ src/components/Filters.js
@@ -4,12 +4,16 @@ import '../styles/components/Filters.scss';
 
 import FilterByBrand from '../components/FilterByBrand';
 import FilterByDepartment from './FilterByDepartment';
+import { MdOutlineSettingsInputComposite } from 'react-icons/md';
 
 function Filters(props) {
   return (
     <form>
       <div className="filter">
-        <h3 className="filter__filtros">filtros</h3>
+        <h3 className="filter__filtros">
+          <MdOutlineSettingsInputComposite className="filter__icon" />
+          filtros
+        </h3>
         <div className="filter__mobileVersion">
           <FilterByBrand
             brands={props.brand}
