42abe04788cab83a786d364b70175cf46723ca5f
Veronica isla <veroislagleaniz@gmail.com>
2022-07-19 11:08:10 +0200

    cleaned code and change css product detail

--- src/components/ProductDetail.js
+++ src/components/ProductDetail.js
@@ -5,15 +5,6 @@ import '../styles/components/ProductDetail.scss';
 import { AiTwotoneStar } from 'react-icons/ai';
 import { GiPresent } from 'react-icons/gi';
 function ProductDetail(props) {
-  const renderSize = () => {
-    return props.getYear.map((year, index) => {
-      return (
-        <option className="filter__box" value={year} key={index}>
-          {year}
-        </option>
-      );
-    });
-  };
   return (
     <>
       {' '}
@@ -21,37 +12,28 @@ function ProductDetail(props) {
         calzado deportivo... / zapatillas deportivas
       </p>
       <div className="productDetail__article">
-        <div className="productDetail__imgPrice">
-          <img
-            className="productDetail__img"
-            src={props.product.models[0].image.url}
-            alt={props.product.models[0].image.url}
-          />
-          <p className="productDetail__webLabel">{props.product.webLabel}</p>
-          <div className="productDetail__starPrice">
-            <div className="productDetail__star">
-              <AiTwotoneStar />
-              <AiTwotoneStar />
-              <AiTwotoneStar />
-              <AiTwotoneStar />
-              <AiTwotoneStar />
-            </div>
-            <p className="productDetail__id">
-              {props.product.models[0].modelId}
-            </p>
-            <p className="productDetail__price">
-              {props.product.models[0].price} €
-            </p>
-
-            <p className="productDetail__present">
-              <GiPresent className="productDetail__present__icon" />
-              ¿Quieres regalar este producto?{' '}
-              <strong className="productDetail__present--strong">
-                Más info
-              </strong>
-            </p>
-          </div>
+        <img
+          className="productDetail__img"
+          src={props.product.models[0].image.url}
+          alt={props.product.models[0].image.url}
+        />
+        <p className="productDetail__webLabel">{props.product.webLabel}</p>
+        <div className="productDetail__star">
+          <AiTwotoneStar />
+          <AiTwotoneStar />
+          <AiTwotoneStar />
+          <AiTwotoneStar />
+          <AiTwotoneStar />
         </div>
+        <p className="productDetail__id">{props.product.models[0].modelId}</p>
+        <p className="productDetail__price">
+          {props.product.models[0].price} €
+        </p>
+        <p className="productDetail__present">
+          <GiPresent className="productDetail__present__icon" />
+          ¿Quieres regalar este producto?{' '}
+          <strong className="productDetail__present--strong">Más info</strong>
+        </p>
       </div>
     </>
   );
